8d643cc76fff47b6d63155ac5cdfc9ce17c8b083
utkarsh sanjay salunke <utkarshssalunke20@gmail.com>
2024-08-05 22:32:12 +0530

    intial commit

---
+++ stranger thinds music/stranger things music in code.rb
@@ -0,0 +1,23 @@
+use_bpm 84
+
+in_thread do
+  live_loop :pedall do
+    with_fx :lpf, cutoff: 60 do
+      use_synth :saw
+      use_synth_defaults release: 0.8
+      play_pattern_timed [:c3,:e3,:g3,:b3,:c4,:b3,:g3,:e3],[0.25]
+    end
+  end
+end
+
+in_thread do
+  use_bpm 84
+  live_loop :pedall do
+    with_fx :lpf, cutoff: 50 do
+      use_synth :prophet
+      use_synth_defaults release: 1.2
+      play_pattern_timed [:c3,:e3,:g3,:b3,:c4,:b3,:g3,:e3],[0.25]
+    end
+  end
+end
+
